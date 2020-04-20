1e993d6b194e50bea1d7b68d6020b18f7e96f846
MiracleHedgehog <panopticon130@gmail.com>
2020-04-20 18:28:05 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # vuetify-color-palette
+
+- `vuetify` で設定したテーマカラーをプレビューするコンポーネント
+
+# env
+
+- `vuetify@2.2.22`
---
+++ VuetifyColorPalette.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-card
+      v-for="(theme, themeName) in themes"
+      :key="themeName"
+      :dark="themeName === 'dark'"
+      :light="themeName === 'light'"
+      class="d-flex pa-5"
+    >
+      <v-card-title class="mr-5">{{ themeName }}</v-card-title>
+
+      <div v-for="(color, key) in theme" :key="key" class="ma-4">
+        <v-card :color="color" height="100px" width="100px" shaped />
+        <div class="mt-1 ml-1">{{ key }}</div>
+      </div>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VuetifyColorPalette",
+  computed: {
+    themes() {
+      return this.$vuetify.theme.themes;
+    }
+  }
+};
+</script>
